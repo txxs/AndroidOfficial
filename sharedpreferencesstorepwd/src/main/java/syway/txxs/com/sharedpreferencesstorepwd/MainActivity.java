9f9a48bfadc2d09821e2deee9200ce1530c1409c
Huimin Jiang <jianghuimin@le.com>
2017-09-06 20:54:32 +0800

    store pwd suc

--- sharedpreferencesstorepwd/src/main/java/syway/txxs/com/sharedpreferencesstorepwd/MainActivity.java
+++ sharedpreferencesstorepwd/src/main/java/syway/txxs/com/sharedpreferencesstorepwd/MainActivity.java
@@ -1,7 +1,10 @@
 package syway.txxs.com.sharedpreferencesstorepwd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
     }
 }
