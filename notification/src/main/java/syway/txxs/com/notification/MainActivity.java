9c865d262c1c2aa72d70f8937fffa00f245e553e
Huimin Jiang <jianghuimin@le.com>
2017-09-07 14:38:17 +0800

    change import not function

--- notification/src/main/java/syway/txxs/com/notification/MainActivity.java
+++ notification/src/main/java/syway/txxs/com/notification/MainActivity.java
@@ -6,8 +6,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
