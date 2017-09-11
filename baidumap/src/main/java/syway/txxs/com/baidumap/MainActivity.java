c2c2fb1eed56c3dc61152fe800f439f0e94a96b4
Huimin Jiang <jianghuimin@le.com>
2017-09-11 21:28:04 +0800

    new

--- baidumap/src/main/java/syway/txxs/com/baidumap/MainActivity.java
+++ baidumap/src/main/java/syway/txxs/com/baidumap/MainActivity.java
@@ -3,16 +3,26 @@ package syway.txxs.com.baidumap;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 
 public class MainActivity extends Activity {
+
+    public LocationClient mLocationClient = null;
+
+    public BDLocationListener myListener = new MyLocationListener();
 
     MapView mMapView = null;
 
@@ -25,7 +35,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.map_view);
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
         BaiduMap mBaiduMap = mMapView.getMap();
+        initLocation();
         mark(mBaiduMap);
     }
     @Override
@@ -55,13 +70,46 @@ public class MainActivity extends Activity {
         //定义Maker坐标点
         LatLng point = new LatLng(39.963175, 116.400244);
         //构建Marker图标
-        BitmapDescriptor bitmap = BitmapDescriptorFactory
-                .fromResource(R.mipmap.ic_launcher);
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
         //构建MarkerOption，用于在地图上添加Marker
-        OverlayOptions option = new MarkerOptions()
-                .position(point)
-                .icon(bitmap);
+        OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
         //在地图上添加Marker，并显示
         mBaiduMap.addOverlay(option);
     }
+
+    private void initLocation(){
+
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+
+        option.setCoorType("bd09ll");
+        //可选，默认gcj02，设置返回的定位结果坐标系
+
+        int span=1000;
+        option.setScanSpan(span);
+        //可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+
+        option.setIsNeedAddress(true);
+        //可选，设置是否需要地址信息，默认不需要
+
+        option.setOpenGps(true);
+        //可选，默认false,设置是否使用gps
+
+        option.setLocationNotify(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+
+        option.setIsNeedLocationDescribe(true);
+        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+
+        option.setIsNeedLocationPoiList(true);
+        //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+
+        option.setIgnoreKillProcess(false);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+    }
+
 }
