bb69d6cf92e9fd18c507dcb239cd0a055d4ed2fb
Huimin Jiang <jianghuimin@le.com>
2017-09-12 17:58:06 +0800

    gaodemap location

--- amaplearn/src/main/java/syway/txxs/com/amaplearn/MainActivity.java
+++ amaplearn/src/main/java/syway/txxs/com/amaplearn/MainActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.MyLocationStyle;
 
 public class MainActivity extends Activity {
 
     private MapView mapView;
     private AMap aMap;
+    private UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,41 @@ public class MainActivity extends Activity {
      * 初始化AMap对象
      */
     private void init() {
+        //显示地图
         if (aMap == null) {
             aMap = mapView.getMap();
         }
+        //显示定位的一些信息
+        location();
+        //界面显示的基本内容
+        uiBasic();
+    }
+
+    public void location(){
+        //定位的一些显示
+        MyLocationStyle myLocationStyle;
+        //初始化定位蓝点样式类
+        myLocationStyle = new MyLocationStyle();
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW );
+        //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(2000);
+        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.mipmap.share);
+        myLocationStyle.myLocationIcon(bitmapDescriptor);
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setMyLocationEnabled(true);
+        //设置缩放级别，数字越大显示的越小3-19
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    public void uiBasic(){
+        //实例化UiSettings类对象
+        mUiSettings = aMap.getUiSettings();
+        //显示指南针
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
     }
 
     @Override
