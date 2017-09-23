7d4bef5fdf9af3473ab1e7840a7f9bbe3f5ec0c0
Huimin Jiang <jianghuimin@le.com>
2017-09-23 16:07:48 +0800

    modify color

--- syway/src/main/java/syway/txxs/com/syway/util/CountDownButtonUtil.java
+++ syway/src/main/java/syway/txxs/com/syway/util/CountDownButtonUtil.java
@@ -1,5 +1,6 @@
 package syway.txxs.com.syway.util;
 
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.widget.Button;
 
@@ -19,13 +20,15 @@ public class CountDownButtonUtil extends CountDownTimer {
     @Override
     public void onTick(long millisUntilFinished) {
         button.setTextSize(14);
-        button.setText(millisUntilFinished / 1000 + "秒后重新发送");
+        button.setText("重新获取" + millisUntilFinished / 1000 + "秒");
+        button.setBackgroundColor(Color.parseColor("#d5d5d5"));
         button.setClickable(false);
     }
 
     @Override
     public void onFinish() {
         button.setText("获取验证码");
+        button.setBackgroundColor(Color.WHITE);
         button.setClickable(true);
     }
 }
