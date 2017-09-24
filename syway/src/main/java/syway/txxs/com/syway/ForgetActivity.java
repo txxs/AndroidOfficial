860bc2056d9755e844d6004c27fc393142f7a493
Huimin Jiang <jianghuimin@le.com>
2017-09-24 12:19:28 +0800

    add forget

---
+++ syway/src/main/java/syway/txxs/com/syway/ForgetActivity.java
@@ -0,0 +1,61 @@
+package syway.txxs.com.syway;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import syway.txxs.com.syway.po.User;
+import syway.txxs.com.syway.util.Constants;
+import syway.txxs.com.syway.util.CountDownButtonUtil;
+import syway.txxs.com.syway.util.ToastUtil;
+import syway.txxs.com.syway.util.ValidateUtil;
+
+/**
+ * Created by jianghuimin on 2017/9/24.
+ */
+
+public class ForgetActivity extends Activity {
+
+    @BindView(R.id.forget_btn_vcode)
+    Button forgetBtnVcode;
+
+    @BindView(R.id.forget_edt_phone)
+    EditText forgetEdtPhone;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget);
+        ButterKnife.bind(this);
+        sendVcode();
+    }
+
+    /**
+     * 发送验证码
+     * 1、验证手机号码
+     * 2、判断是否已经注册
+     * 3、发送验证码
+     */
+    public void sendVcode(){
+        forgetBtnVcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = forgetEdtPhone.getText().toString();
+                if(!ValidateUtil.isPhoneNumber(phoneNumber)){
+                    ToastUtil.setToastProperties(ForgetActivity.this, Constants.PHONE_NUMBER_WRONG_TIPS);
+                    return;
+                }
+                //发送短信验证码,具体发送在后端进行，andriod端控制发送的时间
+                new CountDownButtonUtil(60000,1000,forgetBtnVcode).start();//参数依次为总时长，计时时间间隔
+            }
+        });
+    }
+}
--- syway/src/main/java/syway/txxs/com/syway/LoginActivity.java
+++ syway/src/main/java/syway/txxs/com/syway/LoginActivity.java
@@ -1,8 +1,11 @@
 package syway.txxs.com.syway;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
@@ -17,6 +20,9 @@ public class LoginActivity extends Activity {
     @BindView(R.id.login_edt_phone)
     EditText loginEdtPhone;//注册的手机号
 
+    @BindView(R.id.login_btn_logined_forget)
+    Button loginBtnLoginedForget;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +34,17 @@ public class LoginActivity extends Activity {
         if(phoneNumber!=null){
             loginEdtPhone.setText(phoneNumber);
         }
+        loginToForgt();
+    }
+
+    public void loginToForgt(){
+        loginBtnLoginedForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ForgetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
